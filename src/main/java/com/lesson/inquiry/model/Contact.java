b6ed9ffbb7d2deafd08455f0b082224a2e68bd36
MandokoroTakashi616 <mandokoro-t@cin-group.com>
2026-05-21 12:14:15 +0900

    feat: Lesson6の追加

--- src/main/java/com/lesson/inquiry/model/Contact.java
+++ src/main/java/com/lesson/inquiry/model/Contact.java
@@ -1,4 +1,4 @@
-package com.lesson.memo.model;
+package com.lesson.inquiry.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,11 +9,8 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Memo {
+public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    // フィールドを追加してください
-
 }
