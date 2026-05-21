b29eed808dc03ad97c6ddb0c63a11b355219c262
MandokoroTakashi616 <mandokoro-t@cin-group.com>
2026-05-21 11:12:09 +0900

    first commit

---
+++ src/main/java/com/lesson/memo/FutureforceSpringbootLessonAppApplication.java
@@ -0,0 +1,13 @@
+package com.lesson.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureforceSpringbootLessonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureforceSpringbootLessonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lesson/memo/FutureforceSpringbootLessonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesson.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureforceSpringbootLessonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
