745d8043c36bda97868ad5582eb457a006a1185c
MandokoroTakashi616 <mandokoro-t@cin-group.com>
2026-05-21 11:39:42 +0900

    feat: Lesson1の追加

---
+++ src/main/java/com/lesson/memo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lesson.memo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // 【/hello】にアクセスしたら、Hello, Spring Boot! を返すように記述しましょう
+    // @GetMapping("-----")
+    // public String -----() {
+    //     return "-----";
+    // }
+}
